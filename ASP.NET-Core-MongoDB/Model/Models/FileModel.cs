dfa8ac3aa5e33bf9b48a5b860b510395b2fe2e8d
Ruslan Galeev <ruslan.nafisovich@gmail.com>
2017-06-20 11:35:30 +0500

    Added models

---
+++ ASP.NET-Core-MongoDB/Model/Models/FileModel.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Models
+{
+    public class FileModel
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string IdFileInfo { get; set; }
+
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        // Содержимое FSGrid:
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string IdFileBody { get; set; }
+
+        [BsonIgnore]
+        public byte[] FileBody { get; set; }
+    }
+}
