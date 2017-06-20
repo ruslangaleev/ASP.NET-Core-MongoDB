08d29fd330ab63f03aea8ba8301cc217f640e001
Ruslan Galeev <ruslan.nafisovich@gmail.com>
2017-06-20 11:54:30 +0500

    Addes service

---
+++ ASP.NET-Core-MongoDB/Service/FileService.cs
@@ -0,0 +1,45 @@
+﻿using Data;
+using Model.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IFileService
+    {
+        Task<FileModel> GetFile(string id);
+
+        Task AddFile(FileModel fileInfo);
+
+        Task<DeleteResult> RemoveFile(string id);
+    }
+
+    public class FileService : IFileService
+    {
+        private readonly IFileRepository _fileRepository;
+
+        // Controller
+        public FileService(IFileRepository fileRepository)
+        {
+            _fileRepository = fileRepository;
+        }
+
+        public async Task AddFile(FileModel fileInfo)
+        {
+            await _fileRepository.AddFile(fileInfo);
+        }
+
+        public async Task<FileModel> GetFile(string id)
+        {
+            return await _fileRepository.GetFile(id) ?? new FileModel();
+        }
+
+        public async Task<DeleteResult> RemoveFile(string id)
+        {
+            return await _fileRepository.RemoveFile(id);
+        }
+    }
+}
